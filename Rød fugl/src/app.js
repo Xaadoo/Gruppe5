dad1358b635df2f98b7f11f48816f3a8516b15bf
Lashnan <36000745+Lashnan@users.noreply.github.com>
2018-04-06 11:11:44 +0200

    Add files via upload

--- Rød fugl/src/app.js
+++ Rød fugl/src/app.js
@@ -1,2 +1,379 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// @flow
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import { Link, NavLink, HashRouter, Switch, Route } from 'react-router-dom';
+import createHashHistory from 'history/createHashHistory';
+const history = createHashHistory();
+import { userService, eventService, memberService, roleService, mannskapService } from './innloggingService';
+//skrev Eventa fordi Event er et reservert ord
+
+class ErrorMessage extends React.Component<{}> {
+  refs: {
+    closeButton: HTMLButtonElement
+  };
+
+  message = '';
+
+  render() {
+    // Only show when this.message is not empty
+    let displayValue;
+    if(this.message=='') displayValue = 'none';
+    else displayValue = 'inline';
+
+    return (
+      <div style={{display: displayValue}}>
+        <b><font color='red'>{this.message}</font></b>
+        <button ref='closeButton'>x</button>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    errorMessage = this;
+    this.refs.closeButton.onclick = () => {
+      this.message = '';
+      this.forceUpdate();
+    };
+  }
+
+  componentWillUnmount() {
+    errorMessage = null;
+  }
+
+  set(post: string) {
+    this.message = post;
+    this.forceUpdate();
+  }
+}
+let errorMessage: ?ErrorMessage;
+
+class Menu extends React.Component<{}> {
+  render() {
+    let signedInUser = userService.getSignedInUser();
+    if(signedInUser) {
+      return (
+        <div>
+          <NavLink activeStyle={{color: 'red'}} exact to='/'>Hjem</NavLink>{' '}
+          <NavLink activeStyle={{color: 'red'}} exact to='/event'>Arrangement</NavLink>{' '}
+          <NavLink activeStyle={{color: 'red'}} exact to='/crew'>Mannskap</NavLink>{' '}
+          <NavLink activeStyle={{color: 'red'}} exact to='/roles'>Roller</NavLink>{' '}
+          <NavLink activeStyle={{color: 'red'}} exact to='/mypage'>Min Side</NavLink>{' '}
+          <NavLink activeStyle={{color: 'red'}} to='/signout'>Logg ut</NavLink>{' '}
+        </div>
+      );
+    }
+    return (
+      <div>
+        <NavLink activeStyle={{color: 'red'}} to='/signin'>Logg inn</NavLink>{' '}
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    menu = this;
+  }
+
+  componentWillUnmount() {
+    menu = null;
+  }
+}
+
+let menu: ?Menu;
+
+class SignIn extends React.Component<{}> {
+  refs: {
+    signInUsername: HTMLInputElement,
+    signInPassword: HTMLInputElement,
+    signInButton: HTMLButtonElement
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Innlogging </h1>
+            <input type="text" ref="signInUsername" />
+            <input type="password" ref="signInPassword" />
+          <button ref="signInButton">Logg inn</button>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    if(menu) {
+        menu.forceUpdate();
+    }
+
+    this.refs.signInButton.onclick = () => {
+      userService.signIn(this.refs.signInUsername.value, this.refs.signInPassword.value).then(() => {
+        history.push('/');
+        console.log("Logget inn confirmed!");
+      }).catch((error: Error) => {
+        if(errorMessage) errorMessage.set("Incorrect username");
+      });
+    };
+  }
+}
+
+class Home extends React.Component<{}> {
+    constructor() {
+        super();
+        this.members = [];
+    }
+    render() {
+        let listMembers= [];
+        for(let member of this.members) {
+            listMembers.push(<li key={member.ID}>{member.Fornavn}</li>) //use key prop for optimalization
+        }
+
+        return <div>
+            <h1>Hjem</h1><br/>
+            <div>
+                {listMembers}
+            </div>
+        </div>;
+    }
+
+
+    componentDidMount() {
+        memberService.getMembers().then((members) => {
+            this.members = members;
+            this.forceUpdate();
+        }).catch((error) => {
+            if (errorMessage) errorMessage.set('Error getting members: ' + error.message);
+        });
+        memberList = this;
+    }
+}
+let memberList;
+
+class AddEvent extends React.Component<{}> {
+  refs: {
+    addEventName: HTMLInputElement,
+    addZipCode: HTMLInputElement,
+    addEventStartDate: HTMLInputElement,
+    addEventEndDate: HTMLInputElement,
+    addEventDescription: HTMLInputElement,
+    addStartTime: HTMLInputElement,
+    addEndTime: HTMLInputElement,
+    addMeetingDate: HTMLInputElement,
+    addMeetingPoint: HTMLInputElement,
+    addMeetingTime: HTMLInputElement,
+    addContactPerson: HTMLInputElement,
+    addEventButton: HTMLButtonElement
+  }
+  render() {
+    return <div>
+              <h1>Opprett arrangement</h1><br />
+
+        Navn: <input type="text" ref="addEventName" />
+        Postnummer: <input type="text" ref="addZipCode" />
+        Startdato: <input type="date" ref="addEventStartDate" />
+        Slutdato: <input type="date" ref="addEventEndDate" /><br />
+        Beskrivelse: <textarea cols="40" rows="5" ref='addEventDescription' /><br /><br />
+        Starttidspunkt: <input type="time" ref="addStartTime" />
+        Sluttidspunkt: <input type="time" ref="addEndTime" /><br /><br />
+        Oppmøtedato: <input type="date" ref="addMeetingDate" />
+        Oppmøtested: <input type="text" ref="addMeetingPoint" />
+        Oppmøtetidspunkt: <input type="time" ref="addMeetingTime" /><br />
+        Kontaktperson: <input type="number" ref="addContactPerson" />
+        Vaktmal:<br />
+        Utstyrsliste: <br /><br />
+
+            <button ref="addEventButton">Opprett arrangement</button>
+            </div>
+  }
+
+  componentDidMount() {
+    eventList = this;
+    this.refs.addEventButton.onclick = () => {
+      eventService.addEvent(this.refs.addEventName.value,
+                            this.refs.addZipCode.value,
+                            this.refs.addEventStartDate.value,
+                            this.refs.addEventEndDate.value,
+                            this.refs.addEventDescription.value,
+                            this.refs.addStartTime.value,
+                            this.refs.addEndTime.value,
+                            this.refs.addMeetingDate.value,
+                            this.refs.addMeetingPoint.value,
+                            this.refs.addMeetingTime.value,
+                            this.refs.addContactPerson.value).then((id) => {
+        history.push('/addevent/'+id);
+        console.log("Arrangmenet lagt til!");
+      }).catch((error: Error) => {
+        if(errorMessage) errorMessage.set("Error adding the event.");
+      });
+    };
+  }
+  componentWillUnmount() {
+    eventList = null;
+  }
+}
+
+class Event extends React.Component<{}> {
+
+  constructor() {
+    super();
+    this.events = [];
+  }
+  render() {
+    let listEvents = [];
+    for(let eventa of this.events) {
+        listEvents.push(<li key={eventa.idArrangementer}>{eventa.Arrangement_Navn}</li>) //bruker key prop for optimalisering
+    }
+
+      return <div>
+          <h1>Arrangement</h1><br/>
+          <div>
+              {listEvents}
+          </div>
+          <button ref="goToEventButton">Opprett arrangement</button>
+      </div>;
+  }
+
+
+  componentDidMount() {
+      eventService.getEvents().then((events) => {
+          this.events = events;
+          this.forceUpdate();
+      }).catch((error) => {
+          if (errorMessage) errorMessage.set('Error getting notes: ' + error.message);
+      });
+      eventList = this;
+      this.refs.goToEventButton.onclick = () => {
+          history.push('/addevent');
+          console.log("Hoppet til addevent");
+      };
+  }
+}
+let eventList;
+
+class Crew extends React.Component<{}> {
+  render() {
+    return <div>
+              <h1>Mannskap</h1>
+
+            </div>
+  }
+}
+
+class Roles extends React.Component<{}> {
+    constructor() {
+        super();
+        this.roles = [];
+    }
+  render() {
+      let listRoles = [];
+      for(let role of this.roles) {
+          listRoles.push(<tr key={role.id}>{role.Rolle}<td>{role.Krav}</td></tr>) //får ikke til å vise BÅDE Rollenavn og Kravene i sine egne celler
+      }
+
+    return <div>
+              <h1>Roller</h1>
+                <table>
+                    <tbody>
+                        <td border = "1px">
+                            {listRoles}
+                        </td>
+                    </tbody>
+                </table>
+              <button ref="goToRoleButton">Opprett rolle</button>
+            </div>
+  }
+  componentDidMount() {
+    this.refs.goToRoleButton.onclick = () => {
+      history.push('/addrole');
+      console.log("Hoppet til addrole");
+    };
+      roleService.getRoles().then((roles) => {
+          this.roles = roles;
+          this.forceUpdate();
+      }).catch((error) => {
+          if (errorMessage) errorMessage.set('Error getting roles: ' + error.message);
+      });
+      eventList = this;
+  }
+}
+
+class AddRole extends React.Component<{}> {
+  refs: {
+    roleName: HTMLInputElement,
+    roleDescription: HTMLInputElement,
+    addRoleButton: HTMLButtonElement
+  };
+  render() {
+    return <div>
+              <h1>Opprett rolle</h1><br />
+
+              Navn: <input type="text" ref="roleName" /><br />
+              Beskrivelse: <input type="text" ref="roleDescription" />
+              Vaktmal:<br />
+              Kurs?: <br /><br />
+
+            <button ref="addRoleButton">Opprett rolle</button>
+            </div>
+  }
+}
+
+class MyPage extends React.Component<{}> {
+  render() {
+    return (
+            <div>
+              <h1>Min side</h1>
+                info info info
+            </div>
+    )
+  }
+
+  componentDidMount() {}
+
+  // Called when the this.props-object change while the component is mounted
+  // For instance, when navigating from path /user/1 to /user/2
+  componentWillReceiveProps() {
+    setTimeout(() => { this.componentDidMount(); }, 0); // Enqueue this.componentDidMount() after props has changed
+    }
+  }
+
+class SignOut extends React.Component<{}> {
+  refs: {
+    signOut: HTMLInputElement
+  };
+
+  render() {
+    return (<div><button ref="signOut">Press this button to sign out</button></div>);
+  };
+
+  componentDidMount() {
+    signOut = this;
+    this.refs.signOut.onclick = () => {
+      userService.signOut();
+      history.push("/signin");
+      this.forceUpdate();
+      console.log("Logget ut");
+    };
+  }
+}
+
+let signOut: ?SignOut;
+
+let root = document.getElementById("root");
+if(root) {
+  ReactDOM.render((
+    <HashRouter>
+      <div>
+        <ErrorMessage />
+        <Menu />
+        <Switch>
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path='/signout' component={SignOut} />
+          <Route exact path='/event' component={Event} />
+          <Route exact path='/addevent' component={AddEvent} />
+          <Route exact path='/crew' component={Crew} />
+          <Route exact path='/roles' component={Roles} />
+          <Route exact path='/addrole' component={AddRole} />
+          <Route exact path='/mypage:id' component={MyPage} />
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </div>
+    </HashRouter>
+  ), root);
+};
---
+++ Rød fugl/src/innloggingService.js
@@ -0,0 +1,237 @@
+// @flow
+import * as mysql from 'mysql';
+
+// Setup MySQL-server connection
+let connection;
+function connect() {
+  connection = mysql.createConnection({
+    host     : 'mysql.stud.iie.ntnu.no',
+    user     : 'g_oops_25',  // Replace [username] with your username
+    password : 'XsWrxOxJ',     // Replace [password] with your password
+    database : 'g_oops_25'   // Replace [username] with your username
+  });
+
+// Connect to MySQL-server
+connection.connect(function(error) {
+  if(error) throw error; // If error, show error in console and return from this function
+});
+
+connection.on('error', (error: Error) => {
+  if (error.code === 'PROTOCOL_CONNECTION_LOST') { // Reconnect if connection to server is lost
+    connect();
+  }
+  else {
+    throw error;
+  }
+});
+}
+connect();
+
+class User {
+  id: number;
+  username: string;
+  firstName: string;
+  password: string;
+}
+class RoleService {
+    getRoles() : Promise<void> {
+        return new Promise((resolve, reject) => {
+            connection.query('SELECT * FROM Roller', (error, result) => {
+                if(error) {
+                    reject(error);
+                    return;
+                }
+                resolve(result);
+            });
+        });
+    }
+}
+let roleService = new RoleService;
+
+class MannskapService {
+  getShiftTemplate() : Promise<void> {
+    return new Promise((resolve, reject) => {
+      connection.query('SELECT * FROM Mannskap', (error, result) => {
+        if(error) {
+          reject(error);
+          return;
+          }
+          resolve(result);
+          });
+      });
+  }
+}
+let mannskapService = new MannskapService;
+// Class that performs database queries related to members
+class MemberService {
+    getMembers() : Promise<void> {
+        return new Promise((resolve, reject) => {
+            connection.query('SELECT * FROM Medlemmer', (error, result) => {
+                if(error) {
+                    reject(error);
+                    return;
+                }
+                resolve(result);
+            });
+        });
+    }
+    getMember(id) : Promise<void> {
+        return new Promise((resolve, reject) => {
+            connection.query('SELECT * FROM Medlemmer Where id = ?', [id], (error, result) => {
+                if(error) {
+                    reject(error);
+                    return;
+                }
+                resolve(result[0]);
+            });
+        });
+    }
+
+    changeMembers(id, fornavn, text) : Promise<void>{
+        return new Promise((resolve, reject) => {
+            connection.query('UPDATE Medlemmer SET fornavn = ?, text = ? WHERE id = ?', [fornavn, text, id], (error, result) => {
+                if(error) {
+                    reject(error);
+                    return;
+                }
+                resolve(result);
+            });
+        });
+    }
+    deleteMember(id) : Promise<void> {
+        return new Promise((resolve, reject) => {
+            connection.query("DELETE FROM Medlemmer Where ID = ?", [id], (error, result) => {
+                if(error) {
+                    reject(error);
+                    return;
+                }
+                resolve(result);
+            });
+        });
+    }
+}
+
+let memberService = new MemberService();
+
+class UserService {
+  signIn(username: string, password: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      connection.query( "SELECT * FROM Medlemmer WHERE BINARY Brukernavn=? AND BINARY Passord=?", [username, password], (error, result) => {
+      // connection.query( 'SELECT EXISTS (SELECT * FROM Medlemmer WHERE Brukernavn = ? AND Passord = ?', [username, password], (error, result) => {
+      // connection.query('SELECT * FROM Medlemmer where Brukernavn=?', [username], (error, result) => {
+      // connection.query('SELECT CASE WHEN EXIST ( SELECT * FROM Medlemmer WHERE Brukernavn = ?, Passord = ? ) THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END', [username, password], (error, result) => {
+
+        if(error) {
+          reject(error);
+          return;
+        }
+        if(result.length!=1) {
+          reject(new Error('Result length was not 1'))
+          return;
+        }
+
+        localStorage.setItem('signedInUser', JSON.stringify(result[0])); // Store User-object in browser
+        resolve();
+        console.log("Logget inn!");
+      });
+    });
+  }
+
+  getSignedInUser(): ?User {
+    let item = localStorage.getItem('signedInUser'); // Get User-object from browser
+    if(!item) return null;
+
+    return JSON.parse(item);
+  }
+
+  signOut() {
+    localStorage.setItem('signedInUser', "");
+  }
+
+
+  // getFriends(id: number): Promise<User[]> {
+  //   return new Promise((resolve, reject) => {
+  //     connection.query('SELECT * FROM Users where id!=?', [id], (error, result) => {
+  //       if(error) {
+  //         reject(error);
+  //         return;
+  //       }
+  //
+  //       resolve(result);
+  //     });
+  //   });
+  // }
+}
+
+  let userService = new UserService();
+
+class Eventa {
+  //skrev Eventa fordi Event er et reservert ord
+  id: number;
+  eventName: string;
+  zipCode: number;
+  eventStartDate: string;
+  eventEndDate: string;
+  eventDescription: string;
+  startTime: string;
+  endTime: string;
+  meetingDate: string;
+  meetingPoint: string;
+  meetingTime: string;
+  contactPerson: string;
+}
+
+class EventService {
+  getEvents() : Promise<void> {
+    return new Promise((resolve, reject) => {
+      connection.query('SELECT * FROM Arrangementer', (error, result) => {
+        if(error) {
+          reject(error);
+          return;
+        }
+        resolve(result);
+        });
+      });
+    }
+
+  addEvent(
+      eventName: string,
+      zipCode: string,
+      eventStartDate: Date,
+      eventEndDate: Date,
+      eventDescription: string,
+      startTime: string,
+      endTime: string,
+      meetingDate: Date,
+      meetingPoint: string,
+      meetingTime: string,
+      contactPerson: string)
+    : Promise<void> {
+    return new Promise((resolve, reject) => {
+      connection.query(
+          'INSERT INTO Arrangementer (Postnummer, Beskrivelse, StartDato, SluttDato, StartTid, SluttTid, OppmoteDato,  OppmoteTid, OppmoteSted, EksternKontakt, Arrangement_Navn) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+          [zipCode, eventDescription, eventStartDate, eventEndDate, startTime, endTime, meetingDate, meetingTime, meetingPoint, contactPerson, eventName], (error, result) => {
+              // skriv dette i sluttrapporten: Det var vanskelig å få til denne spørringen da vi slurva med datatyper.
+      // connection.query( 'SELECT EXISTS (SELECT * FROM Medlemmer WHERE Brukernavn = ? AND Passord = ?', [username, password], (error, result) => {
+      // connection.query('SELECT * FROM Medlemmer where Brukernavn=?', [username], (error, result) => {
+      // connection.query('SELECT CASE WHEN EXIST ( SELECT * FROM Medlemmer WHERE Brukernavn = ?, Passord = ? ) THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END', [username, password], (error, result) => {
+
+              if(error) {
+                  reject(error);
+                  return;
+              }
+              // if(typeof(result.insertId) !=='number') {
+              //   reject(new Error('Could not read insertId'))
+              //   return;
+              // }
+
+              resolve(result[1]);
+          });
+    });
+  }
+}
+let eventService = new EventService();
+//skrev Eventa fordi Event er et reservert ord
+  export { User, userService, Eventa, eventService, memberService, roleService };
+
+
